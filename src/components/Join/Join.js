9ea2308e7d7ca231e435d50b01b3a5448b1b5367
samad <samadarshad@hotmail.co.uk>
2021-04-30 14:52:02 +0100

    implementing auth0

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,5 +1,40 @@
 import React, { useState } from 'react'
 import './Join.css'
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+
+    return <button className="button" onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+    const { logout } = useAuth0();
+
+    return (
+        <button className="button mt-20" onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+        </button>
+    );
+};
+
+const Profile = () => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+
+    return (
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+        )
+    );
+};
 
 const Join = ({ signIn }) => {
     const [name, setName] = useState('')
@@ -24,7 +59,10 @@ const Join = ({ signIn }) => {
                 {!token ?
                     <>
                         <h1 className="heading">Chat App</h1>
-                        <button className="button" type="submit" onClick={(event) => authorize(event)}>Sign In</button>
+                        < LoginButton />
+                        < LogoutButton />
+                        <Profile />
+                        {/* <button className="button" type="submit" onClick={(event) => authorize(event)}>Sign In</button> */}
                     </>
                     :
                     <>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
     <Router>
-        <App />
+        <Auth0Provider
+            domain="samadarshad.eu.auth0.com"
+            clientId="VOJMJzaEytuFfdhvlzNf8GUwX5LSLZCv"
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>,
     </Router>
     , document.querySelector('#root'));
