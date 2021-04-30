614730cd6f97311c5c893e03376795a513176293
samad <samadarshad@hotmail.co.uk>
2021-04-30 14:31:04 +0100

    preparing for auth

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -4,6 +4,7 @@ import './Join.css'
 const Join = ({ signIn }) => {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    const [token, setToken] = useState('')
 
     function joinRoom(event) {
         if (name && room) {
@@ -12,14 +13,28 @@ const Join = ({ signIn }) => {
         }
     }
 
+    function authorize(event) {
+
+    }
+
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
-                <button className="button" type="submit" onClick={(event) => joinRoom(event)}>Sign In</button>
-                <div><input placeholder="Name" className="joinInput mt-20" type="text" onChange={(event) => setName(event.target.value)} /></div>
-                <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
-                <button className="button mt-20" type="submit" onClick={(event) => joinRoom(event)}>Join Room</button>
+
+                {!token ?
+                    <>
+                        <h1 className="heading">Chat App</h1>
+                        <button className="button" type="submit" onClick={(event) => authorize(event)}>Sign In</button>
+                    </>
+                    :
+                    <>
+                        <h1 className="heading">Join</h1>
+                        <div><input placeholder="Name" className="joinInput mt-20" type="text" onChange={(event) => setName(event.target.value)} /></div>
+                        <div><input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+                        <button className="button mt-20" type="submit" onClick={(event) => joinRoom(event)}>Join Room</button>
+                    </>
+                }
+
             </div>
         </div>
     )
